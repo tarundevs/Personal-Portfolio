0ca08367bbb873f33799327eb8aff25adf20c472
Tarun Warrier <tarunwarrier1@gmail.com>
2025-06-16 15:52:34 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { ThemeToggle } from "../components/ThemeToggle.jsx";
+import {Starbackground} from "@/components/StarBackground.jsx";
+import {NavBar} from "@/components/NavBar.jsx";
+import {HomeSection} from "@/components/HomeSection.jsx";
+import {AboutUs} from "@/components/AboutUs.jsx";
+import { Help } from "@/components/Help.jsx";
+import {Footer} from "@/components/Footer.jsx"
+export const Home = () => {
+  return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+    {/* Theme Toggle */}
+    <ThemeToggle />
+    {/* Background effects */}
+    <Starbackground />
+    {/* NavBar */}
+    <NavBar />
+    {/* Main Content */}
+    <main>
+        <HomeSection />
+        <AboutUs />
+        <Help />
+    </main>
+    {/* Footer */}
+    <Footer />
+  </div>
+};
---
+++ client/src/pages/SpeechASLPage.jsx
@@ -0,0 +1,21 @@
+import { ThemeToggle } from "../components/ThemeToggle.jsx";
+import { Starbackground } from "@/components/StarBackground.jsx";
+import { NavBar } from "@/components/NavBar.jsx";
+import { Speech_ASL } from "@/components/speechASL.jsx";
+
+export const SpeechASLPage = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background effects */}
+      <Starbackground />
+      {/* NavBar */}
+      <NavBar />
+      {/* Main Content */}
+      <main>
+        <Speech_ASL />
+      </main>
+    </div>
+  );
+};
